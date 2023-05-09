bad9e748225a29bdfa44ff18cc99f58b5a297783
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-05-09 16:24:19 +0200

    outline finished

---
+++ translate.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Translate text</title>
+    <link rel="stylesheet" href="css/translate.css">
+</head>
+
+<body>
+<div id="container">
+    <div id="intro">
+        <h2 class="welcome">Welcome,</h2>
+        <h1 class="question">What do you want to translate?</h1>
+    </div>
+    <div id="buttons">
+        <div class="change">
+            <img class="img" src="img/translate-icon.png" alt="text">
+            <p>Text</p>
+        </div>
+        <div class="connect">
+            <img class="img" src="img/talking-head.png" alt="connect">
+            <p>Connect to Pim</p>
+        </div>
+    </div>
+    <div id="translator">
+        <div id="translate">
+            <div class="languages">
+                <p>English</p>
+            </div>
+            <form method="POST" action="translate.php">
+                <div class="texts">
+                    <textarea class="englishText" placeholder="type to translate"></textarea>
+                </div>
+                <input type="submit" name="submit" value="translate">
+            </form>
+            </div>
+    </div>
+</div>
+
+</body>
+</html>
+
