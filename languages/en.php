f285091b5daea5e7a5f3bcbc16333e1b12f22cc0
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-11 20:53:54 +0200

    added the new language for the website and the ability to switch between them to the about us page

---
+++ languages/en.php
@@ -0,0 +1,20 @@
+<?php
+	$lang = array(
+        "lang_en" => "English",
+		"lang_nl" => "Dutch",
+		"translate" => "translate",
+		"about us" => "about us",
+        "h1" => "Our goal in A.I. P.I.M.",
+		"h2" => "Why A.I. P.I.M.?",
+        "h3" => "About team A",
+		"p1" => "A.I. P.I.M. was created to make learning Dutch easy and interactive for international students arriving to the Netherlands.",
+        "p2" => "You receive a spoken translation in real time of the text or speech inputted.",
+		"p3" => "The voice of P.I.M is a synthesized voice from a real Dutch person so it helps with listening as you get used to the Dutch accent.",
+        "p4" => "As your Dutch level improves, the translations will be given with more complex and sophisticated vocabulary so that you can improve your Dutch vocabulary and maintain more complex and real life like conversations.",
+        "p5" => "Traditional translators can often be boring and feel disconnected from the language-learning experience. With a personalized translator that has a sense of personality, users can have a more engaging and enjoyable learning experience that can help them stay motivated to learn the language.",
+		"p6" => "The project does not involve any form of commercialization or profit-making, so users can access this tool for free without any hidden costs.",
+        "p7" => "Team A consists of seven IT students studying in NHL Stenden, University of Applied Sciences.",
+        "p8" => "The team worked on this project in the 4th period of their first year.",
+        "footer" => "brought to you by team A"
+	);
+?>
---
+++ languages/nl.php
@@ -0,0 +1,20 @@
+<?php
+	$lang = array(
+        "lang_en" => "Engels",
+		"lang_nl" => "Nederland",
+		"translate" => "vertalen",
+		"about us" => "over ons",
+        "h1" => "Ons doel in A.I. P.I.M.",
+		"h2" => "Waarom A.I. P.I.M.?",
+        "h3" => "Over team A",
+		"p1" => "AI PIM is gemaakt om Nederlands leren gemakkelijk en interactief te maken voor internationale studenten die naar Nederland komen.",
+        "p2" => "U ontvangt in realtime een gesproken vertaling van de ingevoerde tekst of spraak.",
+		"p3" => "De stem van P.I.M is een gesynthetiseerde stem van een echte Nederlander, dus het helpt bij het luisteren terwijl je went aan het Nederlandse accent.",
+        "p4" => "Naarmate uw Nederlandse niveau verbetert, zullen de vertalingen worden gegeven met een complexere en geavanceerdere woordenschat, zodat u uw Nederlandse woordenschat kunt verbeteren en meer complexe en levensechte gesprekken kunt voeren.",
+        "p5" => "Traditionele vertalers zijn vaak saai en voelen zich niet betrokken bij het leren van een taal. Met een gepersonaliseerde vertaler die een gevoel van persoonlijkheid heeft, kunnen gebruikers een boeiendere en plezierigere leerervaring hebben die hen kan helpen gemotiveerd te blijven om de taal te leren.",
+		"p6" => "Het project houdt geen enkele vorm van commercialisering of winstoogmerk in, dus gebruikers hebben gratis toegang tot deze tool zonder verborgen kosten.",
+        "p7" => "Team A bestaat uit zeven ICT-studenten die studeren aan NHL Stenden, Hogeschool.",
+        "p8" => "Het team werkte aan dit project in de 4e periode van hun eerste jaar.",
+        "footer" => "aangeboden door team A"
+	);
+?>
