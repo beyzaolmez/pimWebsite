41d2e387d9ab29079cc0e68053c37f28dd1b1f23
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-15 23:00:20 +0200

    finished html

---
+++ selectQuiz.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Select quiz</title>
+    <link rel="stylesheet" href="selectQuiz.css">
+</head>
+
+<body>
+<div id="container">
+    <div id="intro">
+        <h2>Welcome,</h2>
+        <h1>What quiz do you want to take?</h1>
+    </div>
+    <div id="select">
+        <div class="checkLevel">
+            <h2>Check your level</h2>
+            <div class="box1">
+                <h3>Do you want to know your Dutch level?</h3>
+                <p>Take the following quiz:</p>
+                <p>Level quiz</p>
+            </div>
+        </div>
+        <div class="levelQuiz">
+            <h2>Take a quiz</h2>
+            <div class="box2">
+                <h3>Do you already know your Dutch level?</h3>
+                <p>Select a quiz:</p>
+                <ul>
+                    <li>Beginner</li>
+                    <li>Intermediate</li>
+                    <li>Advanced</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
