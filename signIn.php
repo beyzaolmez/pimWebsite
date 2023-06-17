d584ac85b9958252f835cb25fbec600b3ae14c5d
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-18 01:42:26 +0200

    finished frontend

---
+++ signIn.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign in</title>
+        <link rel="stylesheet" href="css/signIn.css">
+    </head>
+
+    <body>
+    <div id="container">
+        <div id="singIn">
+            <h1>Sign-in</h1>
+            <form action="signIn.php" method="POST">
+                <div class="credentials">
+                    <textarea name="email" placeholder="Email"></textarea>
+                    <textarea name="password" placeholder="Password"></textarea>
+                    <label> Forgot your password?</label>
+                </div>
+                <div class="submit">
+                    <input type="submit" value="sign in">
+                </div>
+            </form>
+        </div>
+    </div>
+    </body>
+</html>
