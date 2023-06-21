53567f87efc7f574186c623d48c0d92ce1791703
“Atumah <gerald.atumah@student.nhlstenden.com>
2023-06-21 13:06:19 +0200

    added quiz.php

---
+++ quiz.php
@@ -0,0 +1,174 @@
+<?php
+session_start();
+
+// Function to retrieve quiz questions from Python endpoint
+function retrieveQuizQuestions($level) {
+    $endpoint = "http://127.0.0.1:5000/quiz";
+
+    $curl = curl_init($endpoint);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array("level" => $level)));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    if ($response !== false) {
+        $questions = json_decode($response, true);
+        return $questions;
+    } else {
+        return false;
+    }
+}
+
+// Check if the retry button was clicked
+if (isset($_POST['retry'])) {
+    // Retrieve new quiz questions from the Python endpoint
+    $questions = retrieveQuizQuestions("beginner");
+
+    if ($questions) {
+        // Store the retrieved questions in session
+        $_SESSION['quiz'] = $questions;
+        unset($_SESSION['results']); // Clear previous results
+    }
+}
+
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['retry'])) {
+    $userAnswers = $_POST['answers'];
+    $questions = $_SESSION['quiz'];
+    $correctAnswers = array_column($questions['data'], 'answer');
+
+    $results = array();
+    foreach ($userAnswers as $index => $userAnswer) {
+        $questionText = $questions['data'][$index]['question'];
+        $correctAnswer = $correctAnswers[$index];
+        $isCorrect = strtolower($userAnswer) === strtolower($correctAnswer);
+
+        $result = array(
+            'question' => $questionText,
+            'userAnswer' => $userAnswer,
+            'correctAnswer' => $correctAnswer,
+            'isCorrect' => $isCorrect
+        );
+
+        $results[] = $result;
+    }
+
+    $_SESSION['results'] = $results;
+}
+
+// Retrieve or use the stored quiz questions from session
+$questions = isset($_SESSION['quiz']) ? $_SESSION['quiz'] : retrieveQuizQuestions("beginner");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Quiz</title>
+    <style>
+        .question {
+            margin-bottom: 10px;
+        }
+        .correct {
+            color: green;
+        }
+        .wrong {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+<h2>Welcome Username,</h2>
+<h1>Translate the following Dutch words to English:</h1>
+<form id="quizForm" method="POST">
+    <?php foreach ($questions['data'] as $index => $item) { ?>
+        <div class="question">
+            <label for="question<?php echo $index; ?>">
+                <?php echo $item['question']; ?>
+            </label>
+            <input type="text" name="answers[]" id="question<?php echo $index; ?>" value="<?php echo isset($_SESSION['results'][$index]['userAnswer']) ? $_SESSION['results'][$index]['userAnswer'] : ''; ?>">
+        </div>
+    <?php } ?>
+    <input type="submit" value="Submit">
+    <button type="submit" name="retry" id="retryBtn">Retry</button>
+    <a href="selectQuiz.php">End</a>
+
+</form>
+
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['retry']) && isset($_SESSION['results'])) { ?>
+    <h2>Results:</h2>
+    <div id="resultsDiv">
+        <?php foreach ($_SESSION['results'] as $result) { ?>
+            <p>
+                <strong><?php echo $result['question']; ?></strong><br>
+                <?php if ($result['isCorrect']) { ?>
+                    <span class="correct">Correct!</span>
+                <?php } else { ?>
+                    <span class="wrong">Wrong! The correct answer is: <?php echo $result['correctAnswer']; ?></span>
+                <?php } ?>
+            </p>
+        <?php } ?>
+    </div>
+<?php } ?>
+
+<script>
+    document.getElementById('retryBtn').addEventListener('click', function() {
+        fetch('http://127.0.0.1:5000/quiz', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'level=beginner'
+        })
+            .then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to retrieve quiz questions');
+                }
+            })
+            .then(function(data) {
+                sessionStorage.setItem('quizQuestions', JSON.stringify(data));
+                sessionStorage.removeItem('results');
+                document.getElementById('quizForm').innerHTML = generateQuizQuestions(data);
+            })
+            .catch(function(error) {
+                console.error('Error:', error);
+            });
+    });
+
+    function generateQuizQuestions(questions) {
+        var html = '';
+        questions.data.forEach(function(item, index) {
+            html += '<div class="question">';
+            html += '<label for="question' + index + '">' + item.question + '</label>';
+            html += '<input type="text" name="answers[]" id="question' + index + '" value="">';
+            html += '</div>';
+        });
+        html += '<input type="submit" value="Submit">';
+        html += '<button type="button" id="retryBtn">Retry</button>';
+        return html;
+    }
+
+    // Store user's answers in session before leaving the page
+    window.addEventListener('beforeunload', function() {
+        var answers = [];
+        var textInputs = document.querySelectorAll('input[type="text"]');
+        textInputs.forEach(function(input) {
+            answers.push(input.value);
+        });
+
+        // Send AJAX request to the quiz endpoint
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://127.0.0.1:5000/quiz', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.send('level=beginner');
+
+        // Redirect to quizselect.php
+        window.location.href = 'quizselect.php';
+    });
+</script>
+</body>
+</html>
--- test.php
+++ test.php
@@ -1,115 +1,114 @@
 <?php
-    $questions = [];
+$questions = [];
 
-    if($_SERVER["REQUEST_METHOD"] !== "POST") {
-        $level = "beginner";
-        $endpoint = "http://host.docker.internal:5000/quiz";
+if($_SERVER["REQUEST_METHOD"] !== "POST") {
+    $level = "beginner";
+    $endpoint = "http://host.docker.internal:5000/quiz";
 
-        $curl = curl_init($endpoint);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array("level" => $level)));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $curl = curl_init($endpoint);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query(array("level" => $level)));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        curl_setopt($curl, CURLOPT_VERBOSE, true);
+    curl_setopt($curl, CURLOPT_VERBOSE, true);
 
 
-        $response = curl_exec($curl);
-        curl_close($curl);
+    $response = curl_exec($curl);
+    curl_close($curl);
 
-        if ($response !== false) {
-            $questions = json_decode($response, true);
-        } else {
-            echo "Error: Failed to retrieve quiz questions";
-            exit;
-        }
-        if ($questions === null) {
-            echo "Error: failed to parse quiz questions";
-            exit;
-        }
+    if ($response !== false) {
+        $questions = json_decode($response, true);
+    } else {
+        echo "Error: Failed to retrieve quiz questions";
+        exit;
     }
+    if ($questions === null) {
+        echo "Error: failed to parse quiz questions";
+        exit;
+    }
+}
 
-        ?>
-            <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="UTF-8">
-            <title>Test</title>
-            <link rel="stylesheet" href="css/test.css">
-        </head>
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+    <link rel="stylesheet" href="css/test.css">
+</head>
 
-        <body>
-        <div id="container">
-            <div id="intro">
-                <h2 class="first">Welcome Username,</h2>
-                <h1 class="second">Translate the following Dutch words to English in the provided boxes.
-                    When you finish the quiz, click check to get your result and the correct answers. Good luck!</h1>
-                <p>Tip: make sure the spelling is correct</p>
+<body>
+<div id="container">
+    <div id="intro">
+        <h2 class="first">Welcome Username,</h2>
+        <h1 class="second">Translate the following Dutch words to English in the provided boxes.
+            When you finish the quiz, click check to get your result and the correct answers. Good luck!</h1>
+        <p>Tip: make sure the spelling is correct</p>
+    </div>
+    <form action="test.php" method="POST">
+        <?php foreach($questions['data'] as $index => $item){ ?>
+            <div class="test">
+                <label for="question" <?php echo $index; ?>">
+                <?php echo ((int)$index + 1) . '. ' . $item['question']; ?>
+                </label>
+                <textarea name="answers[]" id="question<?php echo $index; ?>"></textarea>
             </div>
-            <form action="test.php" method="POST">
-                <?php foreach($questions['data'] as $index => $item){ ?>
-                    <div class="test">
-                        <label for="question" <?php echo $index; ?>">
-                            <?php echo ((int)$index + 1) . '. ' . $item['question']; ?>
-                        </label>
-                        <textarea name="answers[]" id="question<?php echo $index; ?>"></textarea>
-                    </div>
-                <?php } ?>
-                <div class="submit">
-                    <input type="submit" value="Check">
-                </div>
-            </form>
-
-            <?php
-
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                //$question = $_POST["question"];
-                $answers = $_POST["answers"];
-
-                $score = 0;
-                $incorrectAnswers = array();
-
-                foreach ($questions['data'] as $index => $item){
-                    $correctAnswer = $item['answer'];
-                    $userAnswer = trim($answers[$index]);
-
-                    if(strtolower($userAnswer) === strtolower($correctAnswer)){
-                        $score++;
-                    }
-                    else{
-                        $incorrectAnswers[] = array(
-                                'questionNumber' => ($index + 1),
-                                'question' => $item['question'],
-                            'correctAnswer' => $correctAnswer,
-                            'userAnswer' => $userAnswer,
-                        );
-                    }
-                }
-                ?>
-                <p class='total'>Your score: <?php echo $score ?> out of <?php echo count($questions['data']);?></p>
-                <button class="button" onclick="showIncorrectAnswers()">Show incorrect answers</button>
-                <div id="incorrectAnswer">
-                    <h3>Incorrect answers: </h3>
-                    <ul>
-                        <?php foreach ($incorrectAnswers as $incorrect){ ?>
-                            <li><?php echo $incorrect['questionNumber'] . '. ' . $incorrect['correctAnswer']; ?></li>
-                        <?php } ?>
-                    </ul>
-                </div>
-                <script>
-                    function showIncorrectAnswers() {
-                        var container = document.getElementById("incorrectAnswer");
-                        container.style.display = "block";
-                    }
-                </script>
-            <?php
-            }
-            ?>
+        <?php } ?>
+        <div class="submit">
+            <input type="submit" value="Check">
         </div>
-    </body>
+    </form>
+
+    <?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //$question = $_POST["question"];
+        $answers = $_POST["answers"];
+
+        $score = 0;
+        $incorrectAnswers = array();
+
+        foreach ($questions['data'] as $index => $item){
+            $correctAnswer = $item['answer'];
+            $userAnswer = trim($answers[$index]);
+
+            if(strtolower($userAnswer) === strtolower($correctAnswer)){
+                $score++;
+            }
+            else{
+                $incorrectAnswers[] = array(
+                    'questionNumber' => ($index + 1),
+                    'question' => $item['question'],
+                    'correctAnswer' => $correctAnswer,
+                    'userAnswer' => $userAnswer,
+                );
+            }
+        }
+        ?>
+        <p class='total'>Your score: <?php echo $score ?> out of <?php echo count($questions['data']);?></p>
+        <button class="button" onclick="showIncorrectAnswers()">Show incorrect answers</button>
+        <div id="incorrectAnswer">
+            <h3>Incorrect answers: </h3>
+            <ul>
+                <?php foreach ($incorrectAnswers as $incorrect){ ?>
+                    <li><?php echo $incorrect['questionNumber'] . '. ' . $incorrect['correctAnswer']; ?></li>
+                <?php } ?>
+            </ul>
+        </div>
+        <script>
+            function showIncorrectAnswers() {
+                var container = document.getElementById("incorrectAnswer");
+                container.style.display = "block";
+            }
+        </script>
+        <?php
+    }
+    ?>
+</div>
+</body>
 </html>
 <?php
 
 ?>
-
 
 
