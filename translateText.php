9f7c9642640cf79c247966cf1fd11af7c8ee1340
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-23 14:33:46 +0200

    updated endpoint

--- translateText.php
+++ translateText.php
@@ -100,7 +100,7 @@
 
         console.log(text)
 
-        var response = await fetch('http://127.0.0.1:5000/translate', {
+        var response = await fetch('http://host.docker.internal:5000/ll/translate', {
             method: 'POST',
             body: formData
         });
