d22a4f510408fc8b6c5a2a0cc64ea530082f78c3
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-06-23 09:23:52 +0200

    solved reset button and added cookies

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
-$questions = [];
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(isset($_POST["retry"]) && $_POST["retry"] == "Retry"){
+        session_destroy();
+        header("Location: test.php");
+    }
+}
+var_dump($_POST);
 if($_SERVER["REQUEST_METHOD"] !== "POST") {
     if(!isset($_SESSION['questions'])){
         $level = "beginner";
@@ -19,103 +24,126 @@ if($_SERVER["REQUEST_METHOD"] !== "POST") {
         curl_close($curl);
 
         if ($response !== false) {
-            $questions = json_decode($response, true);
-            $questions = $_SESSION['questions'];
-        } else {
-            echo "Error: Failed to retrieve quiz questions";
-            exit;
+            $decodedResponse = json_decode($response, true);
+            if($decodedResponse !== null && isset($decodedResponse["data"])){
+                $_SESSION["questions"] = $decodedResponse["data"];
+                $questions = $_SESSION["questions"];
+            }else{
+                echo "Error: Invalid quiz questions format";
+                exit;
+            }
         }
-        if ($questions === null) {
-            echo "Error: failed to parse quiz questions";
+        else {
+            echo "Error: Failed to retrieve quiz questions";
             exit;
         }
     }
     else{
-        $questions = $_SESSION['questions'];
+        $questions = $_SESSION["questions"];
     }
 }
+//var_dump($questions);
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Test</title>
-    <link rel="stylesheet" href="css/test.css">
-</head>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test</title>
+        <link rel="stylesheet" href="css/test.css">
+    </head>
 
-<body>
-<div id="container">
-    <div id="intro">
-        <h2 class="first">Welcome Username,</h2>
-        <h1 class="second">Translate the following Dutch words to English in the provided boxes.
-            When you finish the quiz, click check to get your result and the correct answers. Good luck!</h1>
-        <p>Tip: make sure the spelling is correct</p>
-    </div>
-    <form action="test.php" method="POST">
-        <?php foreach($questions['data'] as $index => $item){?>
-            <div class="test">
-                <label for="question" <?php echo $index; ?>">
-                <?php echo ((int)$index + 1) . '. ' . $item['question']; ?>
-                </label>
-                <textarea name="answers[]" id="question<?php echo $index; ?>"></textarea>
-            </div>
-        <?php } ?>
-        <div class="submit">
-            <input type="submit" value="Check">
+    <body>
+    <div id="container">
+        <div id="intro">
+            <h2 class="first">Welcome Username,</h2>
+            <h1 class="second">Translate the following Dutch words to English in the provided boxes.
+                When you finish the quiz, click check to get your result and the correct answers. Good luck!</h1>
+            <p>Tip: make sure the spelling is correct</p>
         </div>
-    </form>
+        <form action="test.php" method="POST">
+            <?php
+            $questions = $_SESSION["questions"];
+            foreach($questions as $index => $item){?>
+                <div class="test">
+                    <label for="question" <?php echo $index; ?>">
+                    <?php echo ((int)$index + 1) . '. ' . $item["question"]; ?>
+                    </label>
+                    <textarea name="answers[]" id="question<?php echo $index; ?>" value=""></textarea>
+                </div>
+            <?php } ?>
+            <div class="submit">
+                <input type="submit" value="Check">
+            </div>
+            <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    if (isset($_POST["answers"])) {
+                        echo "<input type='submit' name='retry' value='Retry'>";
+                        echo "<input type='submit' name='show' value='Show Answers'>";
+                    }
+                }
+            ?>
+        </form>
 
-    <?php
+        <?php
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $questions = $_SESSION['questions'];
-        var_dump($questions);
-        $answers = $_POST["answers"];
+        if($_SERVER["REQUEST_METHOD"] == "POST" ){
+            if (isset($_POST["answers"])) {
+                $questions = $_SESSION["questions"];
+                $answers = $_POST["answers"];
 
-        $score = 0;
-        $incorrectAnswers = array();
+                $score = 0;
+                $incorrectAnswers = array();
 
-        foreach ($questions['data'] as $index => $item){
-            $correctAnswer = $item['answer'];
-            $userAnswer = trim($answers[$index]);
+                foreach ($questions as $index => $item) {
+                    $correctAnswer = $item["answer"];
+                    $userAnswer = trim($answers[$index]);
 
-            if(strtolower($userAnswer) === strtolower($correctAnswer)){
-                $score++;
-            }
-            else{
-                $incorrectAnswers[] = array(
-                    'questionNumber' => ($index + 1),
-                    'question' => $item['question'],
-                    'correctAnswer' => $correctAnswer,
-                    'userAnswer' => $userAnswer,
-                );
+                    if (strtolower($userAnswer) === strtolower($correctAnswer)) {
+                        $score++;
+                    } else {
+                        $incorrectAnswers[] = array(
+                            'questionNumber' => ($index + 1),
+                            'question' => $item["question"],
+                            'correctAnswer' => $correctAnswer,
+                            'userAnswer' => $userAnswer,
+                        );
+                    }
+                }
+                $qsTotal = count($questions);
+                echo "<p class='total'>Your score: {$score} out of $qsTotal </p>";
+
+            ?>
+
+            <?php
+                if (isset($_POST["show"])) {
+                    echo "<div id='incorrectAnswer'>";
+                    echo "<h3>Correct answers: </h3> <ul>";
+                    foreach ($incorrectAnswers as $incorrect) {
+                        echo "<li>" . $incorrect['questionNumber'] . '. ' . $incorrect['correctAnswer'] . "</li>";
+                    }
+                    echo "</ul></div>";
+                }
+            ?>
+                <!--
+            <div id="incorrectAnswer" style="display: none;">
+                <h3>Correct answers: </h3>
+                <ul>
+                    <?php foreach ($incorrectAnswers as $incorrect) { ?>
+                        <li><?php echo $incorrect['questionNumber'] . '. ' . $incorrect['correctAnswer'];
+                    } ?></li>
+                </ul>
+            </div>
+            -->
+                <script>
+                    function showCorrectAnswers() {
+                        var container = document.getElementById("incorrectAnswer");
+                        container.style.display = "block";
+                    }
+                </script>
+        <?php
             }
         }
         ?>
-        <p class='total'>Your score: <?php echo $score ?> out of <?php echo count($questions['data']);?></p>
-        <button class="button" onclick="showIncorrectAnswers()">Show incorrect answers</button>
-        <div id="incorrectAnswer">
-            <h3>Incorrect answers: </h3>
-            <ul>
-                <?php foreach ($incorrectAnswers as $incorrect){ ?>
-                    <li><?php echo $incorrect['questionNumber'] . '. ' . $incorrect['correctAnswer']; ?></li>
-                <?php } ?>
-            </ul>
-        </div>
-        <script>
-            function showIncorrectAnswers() {
-                var container = document.getElementById("incorrectAnswer");
-                container.style.display = "block";
-            }
-        </script>
-        <?php
-    }
-    ?>
-</div>
-</body>
-</html>
-<?php
-
-?>
-
-
+    </div>
+    </body>
+    </html>
