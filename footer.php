75c151ce41b4463cc9ebdc93d4b369ff264725f1
beyza ölmez <beyza.olmez@student.nhlstenden.com>
2023-06-23 11:37:48 +0200

    updated to footer to a php file and added variables for language switch

--- footer.php
+++ footer.php
@@ -7,14 +7,14 @@
     </div>
     <ul class="options">
         <li class="option">
-        <span class="option-text"><a class="option-text" href="#">HELLO</a></span>
+        <span class="option-text"><a class="option-text" href="#"><?php echo $lang['lang_en']; ?></a></span>
         </li>
         <li class="option">
-        <span class="option-text"><a class="option-text" href="#">BYE</a></span>
+        <span class="option-text"><a class="option-text" href="#"><?php echo $lang['lang_nl']; ?></a></span>
         </li>
     </ul>
     </div>
-    <p> brought to you by team A </p>
+    <p> <?php echo $lang['footer']; ?> </p>
 </footer>
 
 <script src="dropdown.js"></script>
